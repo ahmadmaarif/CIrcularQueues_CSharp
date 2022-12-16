1963dae37fb9e2d5e6f5f1a8eb97d46272463061
Ahmad Maarif <ahmad.maarif.ft21@mail.umy.ac.id>
2022-12-16 17:11:02 +0700

    Add project files.

---
+++ CIrcularQueues_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIrcularQueues_CSharp
+{
+    class Queues
+    {
+        int FRONT, FEAR, max = 5;
+        int[] queue_array = new int[5];
+        publiq Queues()
+
+    }
